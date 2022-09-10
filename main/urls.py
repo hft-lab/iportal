ed6f75c4b18dc92aedf53f170cbd12f3693f04a0
Serhii Skrypnik <eyerediskin@gmail.com>
2022-09-10 12:40:10 +0300

    Remove debug toolbar

--- main/urls.py
+++ main/urls.py
@@ -10,9 +10,3 @@ urlpatterns = [
     path('view', views.index, name='view')
 ]
 
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-    path('__debug__/', include(debug_toolbar.urls)),
-] + urlpatterns
